b43f35bce30fcee21de4bbdd8dfccdea2d0cf46a
dhythm <yuta.okada.20@gmail.com>
2022-10-30 03:30:49 +0900

    feat(app): use ServerComponent

---
+++ app/blog/page.tsx
@@ -0,0 +1,22 @@
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1", {
+    cache: "no-cache",
+  });
+  return res.json();
+}
+
+export default async function Page() {
+  const data = await getData();
+
+  return (
+    <main>
+      <h1>Hello, Next.js!</h1>
+      {data && (
+        <>
+          <p>{data.title}</p>
+          <p>{data.completed ? "done" : "doing"}</p>
+        </>
+      )}
+    </main>
+  );
+}
